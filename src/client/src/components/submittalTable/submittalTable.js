bfa157eb9917a5a2a38438999df54e712461b31b
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-06-19 14:33:55 -0700

    converted class component to function component

--- src/client/src/components/submittalTable/submittalTable.js
+++ src/client/src/components/submittalTable/submittalTable.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './submittalTable.css';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,66 +9,57 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
-class SubmittalRow extends Component {
-  render() {
-    return (
-      <TableRow key={this.props.submittal.id}>
-        <TableCell align="right">
-          {this.props.submittal.submittalNumber}
-        </TableCell>
-        <TableCell>
-          {this.props.submittal.description}
-        </TableCell>
-        <TableCell>
-          <Button variant="contained" color="secondary">
-            Delete
-          </Button>
-        </TableCell>
-      </TableRow>
-    );
-  }
+function SubmittalRow(props) {
+  return (
+    <TableRow>
+      <TableCell align="right">
+        {props.submittal.submittalNumber}
+      </TableCell>
+      <TableCell>
+        {props.submittal.description}
+      </TableCell>
+      <TableCell>
+        <Button variant="contained" color="secondary">
+          Delete
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
 }
 
-class SubmittalTable extends Component {
-  constructor() {
-    super();
-    this.state = {
-      submittals: []
-    }
-  }
+function SubmittalTable() {
+  const [submittals, setSubmittals] = useState([]);
 
-  componentDidMount() {
-    fetch('/submittals')
+  useEffect(() => fetch('/submittals')
     .then(res => res.json())
-    .then(submittals => this.setState({submittals}));
-  }
+    .then((submittals) => setSubmittals(submittals)), []);
 
-  render() {
-    return (
-      <div>
-        <h3>
-          Submittals
-        </h3>
-        <TableContainer component={Paper}>
-          <Table size="small" >
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">Submittal No.</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.submittals.map(submittal => 
-                <SubmittalRow submittal={submittal} />)}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <ul>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>
+        Submittals
+      </h3>
+      <TableContainer component={Paper}>
+        <Table size="small" >
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Submittal No.</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {submittals.map(submittal =>
+              <SubmittalRow
+                submittal={submittal}
+                key={submittal.id} />)}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <ul>
+      </ul>
+    </div>
+  );
 }
 
 export default SubmittalTable;
