58e66828dc3de0daf9b645d380e9f0ce9ef2f386
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-06-20 12:00:23 -0700

    create modal posting

--- src/client/src/components/submittalTable/submittalTable.js
+++ src/client/src/components/submittalTable/submittalTable.js
@@ -13,6 +13,10 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 function SubmittalRow(props) {
   const [isVisible, setVisibilty] = useState(true);
@@ -90,12 +94,108 @@ export default function SubmittalTable() {
     .then(res => res.json())
     .then((submittals) => setSubmittals(submittals)), []);
 
+  const [formValues, setFormValues] = useState({});
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCreate = (event) => {
+    event.preventDefault();
+
+    fetch('/submittal/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formValues)
+    })
+      .then(res => res.json())
+      .then(function (data) {
+        console.log(data.id)
+      })
+      .catch(error => console.log(error));
+
+    setOpen(false);
+  };
+
   return (
     <div>
       <h3>
         Submittals
       </h3>
-      <TableContainer component={Paper}>
+      <div align="left" >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpen}
+          align="left"
+          startIcon={<AddIcon />}>
+          Create Submittal
+        </Button>
+      </div>
+      <Dialog
+        fullWidth={true}
+        maxWidth="md"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <form onSubmit={handleCreate}>
+          <DialogTitle id="form-dialog-title">Create Submittal</DialogTitle>
+          <DialogContent>
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                <TextField
+                  autoFocus
+                  variant="filled" 
+                  margin="normal"
+                  id="submittalNumber"
+                  name="submittalNumber"
+                  label="Submittal No."
+                  type="number"
+                  fullWidth
+                  onChange={handleInputChange}
+                  />
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  variant="filled" 
+                  margin="normal"
+                  id="description"
+                  name="description"
+                  label="Description"
+                  fullWidth
+                  onChange={handleInputChange}
+                  />
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="default">
+              Cancel
+            </Button>
+            <Button color="primary" type="submit">
+              Create
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+      <TableContainer component={Paper} mt={2}>
         <Table size="small" >
           <TableHead>
             <TableRow>
