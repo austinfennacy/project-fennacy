14803164cec5bc6923467f888360d55e05783ad8
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-06-19 17:28:09 -0700

    delete submittal works

--- src/client/src/components/submittalTable/submittalTable.js
+++ src/client/src/components/submittalTable/submittalTable.js
@@ -15,6 +15,7 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 function SubmittalRow(props) {
+  const [isVisible, setVisibilty] = useState(true);
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -28,49 +29,57 @@ function SubmittalRow(props) {
   const handleDelete = () => {
     fetch(`/submittal/${props.submittal.id}`, {
       method: 'DELETE'
-    });
+    })
+      .then(res => res.json())
+      .then(function (data) {
+        if (data.success) {
+          setVisibilty(false);
+        }
+      })
+      .catch(error => console.log(error));
 
     setOpen(false);
   };
 
-  return (
-    <TableRow>
-      <TableCell align="right">
-        {props.submittal.submittalNumber}
-      </TableCell>
-      <TableCell>
-        {props.submittal.description}
-      </TableCell>
-      <TableCell>
-        <Button
-          variant="outlined"
-          color="secondary"
-          onClick={handleClickOpen}
-          startIcon={<DeleteIcon />}>
-          Delete
-        </Button>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Are you sure you would like to delete this submittal?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="default">
-              Cancel
-            </Button>
-            <Button onClick={handleDelete} color="secondary" autoFocus>
-              Delete
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </TableCell>
-    </TableRow>
+  return ( isVisible
+    ? <TableRow>
+        <TableCell align="right">
+          {props.submittal.submittalNumber}
+        </TableCell>
+        <TableCell>
+          {props.submittal.description}
+        </TableCell>
+        <TableCell>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleClickOpen}
+            startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Are you sure you would like to delete this submittal?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="default">
+                Cancel
+              </Button>
+              <Button onClick={handleDelete} color="secondary" autoFocus>
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </TableCell>
+      </TableRow>
+    : null
   );
 }
 
