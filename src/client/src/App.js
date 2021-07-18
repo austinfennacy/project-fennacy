597b8541200ce37c5eec27fb9273b87920845652
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-07-18 16:52:57 -0700

    created <Navbar /> component

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import SubmittalTable from './components/submittalTable/submittalTable';
 import Submittal from './components/submittal/submittal';
+import Navbar from './components/navbar/navbar';
 import Container from '@material-ui/core/Container';
 import { Router } from "@reach/router";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <Container maxWidth="xl">
-        <h1>
-          Project Fennacy
-        </h1>
-        <Router>
+        <Router> 
           <SubmittalTable path="/" />
           <Submittal path="submittal/:id" />
         </Router>
