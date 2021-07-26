4fa544dc518e0fcfd027ae5ae468bb9ea18892d5
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-07-25 17:59:25 -0700

    using times new roman font in pdf

--- src/client/src/components/submittal/submittalPdf.js
+++ src/client/src/components/submittal/submittalPdf.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  pdf: {
+    fontFamily: "TimesNewRoman"
+  }, 
+}))
 
 export default function SubmittalPdf(props) {
   const id = props.id
-
+  const classes = useStyles()
+  
   const [submittal, setSubmittal] = useState([])
   useEffect(() => fetch(`/submittal/${id}`)
     .then(res => res.json())
     .then((submittalJson) => setSubmittal(submittalJson)), [id])
 
   return (
-    <div>
-      <h2>Submittal #{submittal.submittalNumber} ({submittal.description})</h2>
+    <div className={classes.pdf}>
+      <h2>
+        SHOP DRAWING AND SUBMITTAL TRANSMITTAL
+      </h2>
+      <div>
+        FUSD McLane High School CTE
+      </div>
       <br />
-      <div>
-        test
-      </div>
-      <div>
-        test
-      </div>
-      <div>
-        test
-      </div>
-      <div>
-        test
-      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   )
 }
