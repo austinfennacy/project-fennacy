819e63dc253d92744f55fb062d711487bd9a4a08
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2021-07-31 13:18:28 -0700

    updated style for navbar

--- src/client/src/components/navbar/navbar.js
+++ src/client/src/components/navbar/navbar.js
@@ -10,6 +10,10 @@ const useStyles = makeStyles({
   root: {
     flexGrow: 1,
   },
+  appBar: {
+    background: 'linear-gradient(45deg, hsl(4, 100%, 30%) 10%, hsl(18, 100%, 40%) 90%)',
+    boxShadow: '0 0px 5px 2px hsl(4, 30%, 70%)',
+  },
 });
 
 export default function Navbar() {
@@ -17,7 +21,7 @@ export default function Navbar() {
   
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar variant="dense">
           <NavDrawer />
           <Typography variant="h6" color="inherit">
